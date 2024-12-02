8579e12a944ccd6b19dff68c0382d2227184292b
Colton Hurst <colton@coltonhurst.com>
2024-12-02 12:26:46 -0500

    Add solution for 24-2-1

--- crates/runner/src/main.rs
+++ crates/runner/src/main.rs
@@ -17,6 +17,18 @@ fn twenty_four_solutions() {
         "24-1-2: {:?}",
         twenty_four::day_one::part_two(day_one_input)
     );
+
+    // Day 2
+    let day_two_file = Path::new("./input-files/24-2.txt");
+    let day_two_input = read_input_file(day_two_file);
+    println!(
+        "24-2-1: {:?}",
+        twenty_four::day_two::part_one(day_two_input.clone())
+    );
+    // println!(
+    //     "24-2-2: {:?}",
+    //     twenty_four::day_two::part_two(day_two_input)
+    // );
 }
 
 #[allow(dead_code)]
