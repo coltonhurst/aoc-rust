3871237a845c67f193affbf6ec6965903a7522d3
Colton Hurst <colton@coltonhurst.com>
2024-12-05 00:20:13 -0500

    Add 24-5-1 parsing code

--- crates/twenty-four/src/day_five.rs
+++ crates/twenty-four/src/day_five.rs
@@ -1,7 +1,29 @@
 /*
-
+    Given a set of rules and input,
+    determine the correctly-ordered
+    updates. Sum the middle page
+    number.
 */
 pub fn part_one(input: Vec<String>) -> i32 {
+    let mut rules: Vec<(i32, i32)> = Vec::new();
+    let mut updates: Vec<Vec<i32>> = Vec::new();
+
+    // Parse the rules & updates from the input
+    let mut first_half = true;
+    for line in input {
+        if line.len() == 0 {
+            first_half = false;
+        }
+
+        if first_half {
+            let line_nums: Vec<i32> = line.split('|').map(|num| num.parse::<i32>().unwrap()).collect();
+            rules.push((line_nums[0], line_nums[1]));
+        } else {
+            let line_nums: Vec<i32> = line.split(',').map(|num| num.parse::<i32>().unwrap()).collect();
+            updates.push(line_nums);
+        }
+    }
+
     0
 }
 
