77c1c9ba1af958f2958c26bbad096ad326f43cab
Colton Hurst <colton@coltonhurst.com>
2024-12-06 10:43:35 -0500

    Add 24-5-2 solution

--- crates/twenty-four/src/day_five.rs
+++ crates/twenty-four/src/day_five.rs
@@ -101,6 +101,12 @@ fn update_is_valid(update: &Vec<i32>, rules: &HashMap<i32, Vec<i32>>) -> bool {
             continue;
         }
 
+        // If there is no rule for this #
+        // then it's valid
+        if !rules.contains_key(number) {
+            continue;
+        }
+
         // Create a vec of the items to the left
         let left_of_num = update[0..number_index].to_vec();
 
@@ -137,7 +143,43 @@ fn update_is_valid(update: &Vec<i32>, rules: &HashMap<i32, Vec<i32>>) -> bool {
       Post Update: 29, 75, 53, 13, 47, 61
 */
 fn sort_update(update: Vec<i32>, rules: &HashMap<i32, Vec<i32>>) -> Vec<i32> {
-    update
+    let mut modified_update = update.clone();
+
+    // Continue looping until `modified_update` is sorted
+    while !update_is_valid(&modified_update, rules) {
+        // Loop through each number in `modified_update`
+        for index in 0..modified_update.len() {
+            // Skip the first number
+            if index == 0 {
+                continue;
+            }
+
+            // If there is no rule for this #
+            // then it's valid
+            if !rules.contains_key(&modified_update[index]) {
+                continue;
+            }
+
+            // Create a vec of the items to the left
+            let left_of_num = modified_update[0..index].to_vec();
+
+            // Create a vec of the applicable rule numbers that should be to
+            // the right of the number
+            let mut applicable_ruleset = rules[&modified_update[index]].clone();
+
+            // If any number from the rule set is to the left,
+            // it's invalid, so swap the current num with the
+            // num to the left
+            applicable_ruleset.retain(|n| left_of_num.contains(n));
+            if applicable_ruleset.len() > 0 {
+                let num_to_left = modified_update[index - 1];
+                modified_update[index - 1] = modified_update[index];
+                modified_update[index] = num_to_left;
+            }
+        }
+    }
+
+    modified_update
 }
 
 #[cfg(test)]
@@ -147,12 +189,12 @@ mod tests {
     #[test]
     fn part_two_success() {
         let pre_update = vec![13, 75, 47, 61, 53, 29];
-        let post_update = vec![29, 75, 53, 13, 47, 61];
+        let expected = vec![29, 75, 53, 13, 47, 61];
         let mut rules: HashMap<i32, Vec<i32>> = HashMap::new();
         rules.insert(75, vec![13]);
         rules.insert(53, vec![13]);
         rules.insert(29, vec![61, 75]);
 
-        assert_eq!(post_update, sort_update(pre_update, &rules));
+        assert_eq!(expected, sort_update(pre_update, &rules));
     }
 }
